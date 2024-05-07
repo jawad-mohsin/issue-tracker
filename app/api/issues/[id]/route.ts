2d2d7fb20c97128109f0c90756b2a53e11c677ea
Jawad Mohsin <jawadmohsin1147@gmail.com>
2024-05-07 11:36:51 +0500

    created edit issue api

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,17 +1,31 @@
-import { createIssueSchema } from "../../../validationSchema";
+import { issueSchema } from "../../../validationSchema";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 
-export async function PUT(req: NextRequest) {
+interface Props {
+  params: { id: string };
+}
+
+export async function PATCH(request: NextRequest, { params: { id } }: Props) {
   try {
-    const { id, title, description, status } = await req.json();
+    const body = await request.json();
+    const validation = issueSchema.safeParse(body);
+    if (!validation.success) {
+      return NextResponse.json(validation.error.format(), { status: 400 });
+    }
 
     const updatedIssue = await prisma.issue.update({
-      where: { id: id },
-      data: { title, description, status },
+      where: { id: parseInt(id) },
+      data: {
+        title: body.title,
+        description: body.description,
+      },
     });
-
-    return NextResponse.json(updatedIssue, { status: 200 });
+    if (updatedIssue) {
+      return NextResponse.json(updatedIssue, { status: 200 });
+    } else {
+      return NextResponse.json({ message: "Invalid issue" }, { status: 404 });
+    }
   } catch (error) {
     return NextResponse.json(
       { message: "Failed to update issue" },
